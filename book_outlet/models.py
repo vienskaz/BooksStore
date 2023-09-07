c2d3ebe0e4d27dd391a75381bd424d98c3d77297
Adam <adwiesek@wp.pl>
2023-09-07 17:46:45 +0200

    My first commit

---
+++ book_outlet/models.py
@@ -0,0 +1,65 @@
+from django.core import validators
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
+
+# Create your models here.
+
+class Country(models.Model):
+    name=models.CharField(max_length=70)
+    code=models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name_plural="Countries"
+
+    
+    def __str__(self):
+        return f"{self.name} {self.code}"
+
+
+
+
+class Address(models.Model):
+    street=models.CharField(max_length=80)
+    postal_code=models.CharField(max_length=7)
+    city = models.CharField(max_length=40)
+
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code},{self.city}"
+    
+    class Meta:
+        verbose_name_plural="Address Entries"
+    
+
+class Author(models.Model):
+    first_name=models.CharField(max_length=100)
+    second_name=models.CharField(max_length=100)
+    address=models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+
+
+
+    def __str__(self):
+        return self.full_name()
+    
+    def full_name(self):
+        return f"{self.first_name} {self.second_name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
+    is_bestselling = models.BooleanField(default=False)
+    # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries =models.ManyToManyField(Country)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
+    
+
